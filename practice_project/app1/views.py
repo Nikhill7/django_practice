867d4099e2b09b5d417874a83261ba12907dc467
nikhil <nikhilwagh@email.com>
2022-05-27 20:16:29 +0530

    Creating new branch and pushing all the code in it

--- practice_project/app1/views.py
+++ practice_project/app1/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    l = ["qwerty","sd", "we"]
+    d = {"name":l}
+    return render(request,'index.html', d)
+
+def link(request):
+    return render(request,'link.html')
